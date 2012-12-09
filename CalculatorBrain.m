f72f8aa4b829caa007378bccb396a84475317f75
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2012-12-09 12:29:06 +1100

    Implemented calculator brain

--- CalculatorBrain.m
+++ CalculatorBrain.m
@@ -8,6 +8,43 @@
 
 #import "CalculatorBrain.h"
 
+@interface CalculatorBrain()
+@property (nonatomic, strong) NSMutableArray *operandStack;
+@end
+
 @implementation CalculatorBrain
 
+- (NSMutableArray *) operandStack {
+    if (!_operandStack) _operandStack = [[NSMutableArray alloc] init]; 
+    return _operandStack;
+}
+
+- (void)pushOperand:(double)operand {
+    [self.operandStack addObject:[NSNumber numberWithDouble:operand]];
+}
+
+- (double)popOperand {
+    NSNumber *operandObject = [self.operandStack lastObject];
+    if (operandObject) [self.operandStack removeLastObject];
+    return  [operandObject doubleValue];
+}
+
+- (double)performOperation:(NSString *)operation {
+    double result = 0;
+    
+    if ([@"+" isEqualToString:operation]) {
+        result = [self popOperand] + [self popOperand];
+    } else if ([@"−" isEqualToString:operation]) {
+        result = [self popOperand] - [self popOperand];
+    } else if ([@"×" isEqualToString:operation]) {
+        result = [self popOperand] * [self popOperand];
+    } else if ([@"÷" isEqualToString:operation]) {
+        result = [self popOperand] / [self popOperand];
+    }
+    
+    [self pushOperand:result];
+    
+    return result; 
+}
+ 
 @end
