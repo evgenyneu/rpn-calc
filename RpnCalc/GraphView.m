cb430fa27e93b02286b149a0494c303ebd5fd225
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2012-12-31 09:57:36 +1100

    Added graph view

---
+++ RpnCalc/GraphView.m
@@ -0,0 +1,31 @@
+//
+//  GraphView.m
+//  RpnCalc
+//
+//  Created by Evgenii Neumerzhitckii on 31/12/12.
+//  Copyright (c) 2012 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import "GraphView.h"
+
+@implementation GraphView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
