345865bec99159a47a634f7fb9c075bc74397387
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-01-01 12:13:40 +1100

    Add scale and origin as properties of Graph view

--- RpnCalc/GraphView.m
+++ RpnCalc/GraphView.m
@@ -10,14 +10,30 @@
 #import "AxesDrawer.h"
 
 @interface GraphView()
+#define DEFAULT_SCALE 10.0
+@property (nonatomic) CGFloat scale;
+@property (nonatomic) CGPoint origin;
 @end
 
 @implementation GraphView
 
+- (CGPoint) origin {
+    if (!_origin.x) {
+        _origin = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
+    }
+    return _origin;
+}
+
+- (CGFloat) scale {
+    if (!_scale) {
+        _scale = DEFAULT_SCALE;
+    }
+    return _scale;
+}
+
 - (void)drawRect:(CGRect)rect
 {
-    CGPoint axesOrigin = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
-    [AxesDrawer drawAxesInRect:self.bounds originAtPoint:axesOrigin scale:10.0];
+    [AxesDrawer drawAxesInRect:self.bounds originAtPoint:self.origin scale:self.scale];
 }
 
 @end
